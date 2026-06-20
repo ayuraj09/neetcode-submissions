a641e6d491da34e9c242067cc714a948ea2ecfe7
Ayush Raj <105922280+ayuraj09@users.noreply.github.com>
2026-06-20 13:41:14 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    vector<string> solve(string &digits, unordered_map<char,string> &mp, string temp, vector<string> &ans, int idx){
+        if(temp.size()==digits.size()){
+            ans.push_back(temp);
+            return ans;
+        }
+        for(auto &it : mp[digits[idx]]){
+            solve(digits,mp,temp+it,ans,idx+1);
+        }
+        return ans;
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0) return {};
+        unordered_map<char,string> mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        vector<string> ans;
+        return solve(digits,mp,"",ans,0);
+    }
+};
